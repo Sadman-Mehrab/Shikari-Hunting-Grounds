0a5c4cebb94a38a334c964f78aa47ae2dcd0a032
Sadman Mehrab <sadmannaqueeb@gmail.com>
2022-12-30 17:44:39 +0600

    Add files via upload

---
+++ Info.cs
@@ -0,0 +1,10 @@
+namespace Shikari
+{
+    internal static class Info
+    {
+        static void Main()
+        {
+            Console.WriteLine("Actual Project Is In The Shikari.zip File");
+        }
+    }
+}
